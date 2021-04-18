a9001db49eb931bccf96b46780136d0cdeb14e73
Cecilia Cruz <cruzscecilia@gmail.com>
2021-04-18 14:30:25 -0700

    Finished adding races

--- src/react/Colors.js
+++ src/react/Colors.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Colors() {
-    const colors = ["Blue", "Pink", "Brown", "Black", "Purple", "Blonde", "Orange", "Red", "Violet", "Green", "Gray", "Gold", "White", "Dark Blue", "Magenta", "Dark Brown", "Dark Purple", "Dark Red", "Dark Green", "Light Blue", "Light Pink", "Light Brown", "Lilac", "Light Blonde", "Light Orange", "Light Red", "Light Green", "Teal", "Lime Green", "Red-Orange"];
+    const colors = ["Blue", "Pink", "Brown", "Black", "Purple", "Blonde", "Orange", "Red", "Violet", "Green", "Gray", "Gold", "White", "Dark Blue", "Magenta", "Dark Brown", "Dark Purple", "Dark Red", "Dark Green", "Light Blue", "Light Pink", "Light Brown", "Lilac", "Light Blonde", "Light Orange", "Light Red", "Light Green", "Teal", "Lime Green", "Red-Orange", "bald"];
     const randNum = Math.floor(Math.random() * colors.length);
     return colors[randNum];
 }
