4ece8764355af7995511c74c409810e808a21fd8
Cecilia Cruz <cruzscecilia@gmail.com>
2021-04-18 11:40:47 -0700

    Finished adding gradient variants

--- src/react/App.js
+++ src/react/App.js
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
-import MainBox from './MainBox.js';
-import Button from './Button.js';
+import {Gradients} from './Colors.js';
+import {FetchInfo} from './IndexNumber.js';
+import FetchRaces from './Races.js';
+import SetGender from './Gender.js';
+import SetHeight from './Height.js';
+import SetHair from './Hair.js'
 
-function App() {
-  return (
-    <div className="background">
-      <div className="header">This is your NPC</div>
-      <MainBox />
-      <Button />
-    </div>
-  )
+export function SetGradient() {
+  let setGradient = Gradients();
+  return setGradient;
+}
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      let setGradient = SetGradient();
+      console.log(setGradient[1]);
+      this.state = {
+          gradient: setGradient,
+          shadow: `0px 0px 30px ${setGradient[1]}`
+      }
+  }
+
+  
+
+  whenLoad = (e) => {
+      this.setState({
+          colorOne: "red"
+      })
+  }
+  
+  render () {
+      return (
+          <div className="background" style={{background: `linear-gradient(${this.state.gradient})`}}>
+              <div className="header" style={{boxShadow: this.state.shadow}}>This is your NPC</div>
+                <FetchInfo>
+                  <div className="container" style={{boxShadow: this.state.shadow}}>
+                <div className="row">
+                      <div className="label">Race</div>
+                      <FetchRaces/>
+                  </div>
+                  <div className="row">
+                      <div className="label">Gender</div>
+                      <SetGender />
+                  </div>
+                  <div className="row">
+                      <div className="label">Height</div>
+                      <SetHeight />
+                  </div>
+                  <div className="row">
+                      <div className="label">Hair</div>
+                      <SetHair />
+                  </div>
+                  <div className="row">
+                      <div className="label">Trait</div>
+                  </div>
+                  <div className="row">
+                      <div className="label">Quirk</div>
+                  </div>
+                  <div className="row">
+                      <div className="label">Hobby</div>
+                  </div>
+                  <div className="row">
+                      <div className="label">Attitude Toward PC</div>
+                  </div>
+                  <div className="row">
+                      <div className="label">Profession</div>
+                  </div>
+              </div>
+            </FetchInfo>
+            <div className="btn" style={{boxShadow: this.state.shadow}}>Randomize</div>
+          </div>
+      )
+  }
 }
 
 
--- src/react/Colors.js
+++ src/react/Colors.js
@@ -1,9 +1,59 @@
 import React from 'react';
 
-function Colors() {
+export default function Colors() {
     const colors = ["Blue", "Pink", "Brown", "Black", "Purple", "Blonde", "Orange", "Red", "Violet", "Green", "Gray", "Gold", "White", "Dark Blue", "Magenta", "Dark Brown", "Dark Purple", "Dark Red", "Dark Green", "Light Blue", "Light Pink", "Light Brown", "Lilac", "Light Blonde", "Light Orange", "Light Red", "Light Green", "Teal", "Lime Green", "Red-Orange"];
     const randNum = Math.floor(Math.random() * colors.length);
     return colors[randNum];
 }
 
-export default Colors;
+export function Gradients() {
+    const randNum = Math.floor(Math.random() * 9);
+    let gradient;
+    switch (randNum) {
+        case 0:
+            gradient = ["#e9be60", "#04442f"];
+            break;
+        case 1:
+            gradient = ["#60bee9", "#043044"];
+            break;
+        case 2:
+            gradient = ["#d0b2e4", "#010241"];
+            break;
+        case 3:
+            gradient = ["#dd4376", "#1e075c"];
+            break;
+        case 4:
+            gradient = ["#dd9a43", "#970000"];
+            break;
+        case 5:
+            gradient = ["#ff5e62", "#610035"];
+            break;
+        case 6:
+            gradient = ["#d9a7c7", "#510061"];
+            break;
+        case 7:
+            gradient= ["#FF6B6B", "#142a42"];
+            break;
+        case 8:
+            gradient = ["#F0C27B", "#4B1248"];
+            break;
+        case 9:
+            gradient = ["#870000", "#190A05"];
+            break;
+        case 10:
+            gradient = ["#A8CABA", "#5D4157"];
+            break;
+        case 11:
+            gradient = ["#e9d362", "#333333"];
+            break;
+        case 12:
+            gradient = ["#49a09d", "#5f2c82"];
+            break;
+
+    }
+
+    return gradient;
+}
+
+
+
